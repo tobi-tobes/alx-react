d428a54b596679af53e449acadd5051a61a03b8d
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-23 15:43:41 -0400

    task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id=\'count\'></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let clickCount = 0;
+
+  function updateCounter() {
+    clickCount += 1;
+    $('#count').text(`${clickCount} clicks on the button`);
+  }
+
+  const debounceupdateCounter = _.debounce(updateCounter, 300);
+
+  $('button').on('click', debounceupdateCounter);
+})
---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Output",
+    }),
+  ],
+};
