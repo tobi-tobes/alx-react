a99da437d745b8d22c978c070c5597a44e1bc3b4
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-23 16:03:26 -0400

    Task 1 fixes

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
@@ -9,9 +9,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack Output",
-    }),
-  ],
 };
