a4d7490f256a97c3265e4aad320296dab9f6c747
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-23 17:28:34 -0400

    Task 1: changes to dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 $(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Holberton Dashoard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id=\'count\'></p>');
