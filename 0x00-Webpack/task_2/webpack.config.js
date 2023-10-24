29db4a0be151f9e7bac244df0aa0403f84567d01
Tobi Tijani <tobi_tijani@yahoo.com>
2023-10-23 22:26:50 -0400

    Task 2 update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,33 +1,26 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
-      { 
+      {
         test: /\.css$/,
-        include: path.resolve(__dirname, './css'),
-        use: ["style-loader", "css-loader"] 
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },
-      { 
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        include: path.resolve(__dirname, './assets'),
-        type: 'asset/resource',
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
       },
-    ]
+    ],
   },
-  performance: {
-    maxAssetSize: 500000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Task 2",
-    }),
-  ],
 };
