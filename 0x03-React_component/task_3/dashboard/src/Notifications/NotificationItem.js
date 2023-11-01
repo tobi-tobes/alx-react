5c733e80ab02a5f030b872cfa7f3759283b47667
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-01 10:47:41 -0400

    Tasks 3-5

---
+++ 0x03-React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.Component {
+  static propTypes = {
+    html: PropTypes.shape({
+      __html: PropTypes.string,
+    }),
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    id: PropTypes.number,
+    markAsRead: PropTypes.func.isRequired,
+  };
+  
+  static defaultProps = {
+    type: 'default',
+  }
+
+  render() {
+    return (
+      <li data-notification-type={ this.props.type } dangerouslySetInnerHTML={ this.props.html } onClick={() => this.props.markAsRead(this.props.id)}>
+          { this.props.value }
+      </li>
+    );
+  }
+}
+
+export default NotificationItem;
