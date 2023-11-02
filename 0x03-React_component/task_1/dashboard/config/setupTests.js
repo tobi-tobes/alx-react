0b0a2032df3ec4ee2d1a72e17e9279ab1831ae4d
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-02 06:12:15 -0400

    Task 1 test

--- 0x03-React_component/task_1/dashboard/config/setupTests.js
+++ 0x03-React_component/task_1/dashboard/config/setupTests.js
@@ -1,14 +1,4 @@
-import encoding from 'text-encoding';
-
-const decoder = new encoding.TextEncoder();
-
-// Now import the Adapter
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { jsdom } from 'jsdom';
 
 configure({ adapter: new Adapter() });
-
-const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-global.document = jsdom(documentHTML);
-global.window = document.parentWindow;
