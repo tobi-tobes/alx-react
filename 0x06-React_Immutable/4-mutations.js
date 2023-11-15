e4eb300823cde56af2a9e0e420caa6b8859b5dc1
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-15 10:07:46 -0500

    Task 4 fix

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,4 +1,11 @@
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
-export const map = fromJS(object);
+export const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
 export const map2 = map.set(1, 'Benjamin').set(4, 'Oliver');
