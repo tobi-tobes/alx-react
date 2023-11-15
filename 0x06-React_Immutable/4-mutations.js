066e282601ac825c682b8f37f47b8336083d5849
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-15 10:14:22 -0500

    Task 4 update: set wrong index

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -8,4 +8,5 @@ export const map = Map({
   5: 'Jacob',
   6: 'Lucas',
 });
-export const map2 = map.set(1, 'Benjamin').set(4, 'Oliver');
+
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
