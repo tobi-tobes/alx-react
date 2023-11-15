79e39a5d938620e82b24ed08eb2ecfef4d9f2328
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-15 10:33:12 -0500

    Task 5

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,18 @@
+import { List, Map } from 'immutable';
+
+export function concatElements(page1, page2) {
+  const list1 = List(page1);
+  const list2 = List(page2);
+  const concat = list1.concat(list2);
+
+  return concat;
+}
+
+export function mergeElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  const merged = map1.merge(map2);
+  const mergedList = List(merged.values());
+
+  return mergedList;
+}
