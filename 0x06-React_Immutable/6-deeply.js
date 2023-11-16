aa49efae0c9f5023c3dbb6c4ab3b716f6ad03d02
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-16 04:27:11 -0500

    Task 6

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -4,6 +4,5 @@ export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
 
-  map1.mergeDeep(map2);
-  return map1.toList();
+  return map1.mergeDeep(map2);
 }
