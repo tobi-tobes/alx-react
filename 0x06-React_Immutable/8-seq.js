410dd9f710eb564d8d55b787d7e092534bd4c030
Tobi Tijani <tobi_tijani@yahoo.com>
2023-11-16 04:58:14 -0500

    Task 8 fix

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -3,7 +3,7 @@ import { Seq } from 'immutable';
 export default function printBestStudents(object) {
   const iter = Seq(object);
   const toPrint = iter
-    .filter((student) => student.score > 70)
+    .filter((student) => student.score >= 70)
     .map((student) => student
       .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
       .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1)));
